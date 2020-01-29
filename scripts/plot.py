cf3e7863472cda2498aed963b08ab42bdec8e383
huweilong <huweilong@whu.edu.cn>
2020-01-29 11:52:45 +0800

    use seaborn

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,31 +1,45 @@
 import numpy as np
 import os
 import argparse
-import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
+sns.set(style="whitegrid")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', default='conll', help="Directory containing the dataset")
 
 
-def plot(files, model_dir, labels):
+def lineplot(files, model_dir, labels):
 
-    color = ['cornflowerblue', 'turquoise', 'darkorange', 'red', 'teal', 'blueviolet', 'black', 'green', 'slategray', 'brown']
-    markers = ['<', 's', 'o', '*', 'x', '^', 'd', 'v', 'p', 'h', '1', '2', '3', '4', '>']
+    values = [np.load(os.path.join(model_dir, npy_file)) for npy_file in files]
+    values = np.array(values).transpose()
 
-    plt.clf()
-    for i, npy_file in enumerate(files):
-        f1s = np.load(os.path.join(model_dir, npy_file))
-        batchs = np.arange(1, len(f1s) + 1)
-        plt.plot(batchs, f1s, color = color[i], marker=markers[i], markevery=10, lw=1, label=labels[i])
+    x = np.arange(len(values))
+    data = pd.DataFrame(values, x, columns=labels)
+    ax = sns.lineplot(data=data, palette="tab10", linewidth=2)
+    ax.set(ylim=(0.6, 1.0), xlim=(10, 60))
+    ax.set(xlabel='Batch', ylabel='Val F1')
+    ax.get_figure().savefig(os.path.join(model_dir, 'f1.png'))
 
-    plt.xlabel('Batch')
-    plt.ylabel('Val F1')
-    plt.ylim([0.0, 1.0])
-    plt.xlim([0, 70])
-    plt.legend(loc="lower right")
-    plt.grid(True)
-    plt.savefig(os.path.join(model_dir, 'f1.png'))
+
+def barplot(labels):
+
+    # Load the example Titanic dataset
+    titanic = sns.load_dataset("titanic")
+
+    # Draw a nested barplot to show survival for class and sex
+    g = sns.catplot(x="class", y="survived", hue="sex", data=titanic,
+                    height=6, kind="bar", palette="muted")
+    g.despine(left=True)
+    g.set_ylabels("survival probability")
+
+
+def to_csv(self, files, labels):
+    values = [np.load(os.path.join(model_dir, npy_file)) for npy_file in files]
+    batches = [np.arange(len(values[0]))]
+    values = np.array(batches + values).transpose()
+    df = pd.DataFrame(data=values, columns=['Batch'] + labels)
 
 
 if __name__ == '__main__':
@@ -35,7 +49,10 @@ if __name__ == '__main__':
     label_map = {
         'random_select': 'Random',
         'least_confidence': 'Least',
-        'token_entropy': 'Entropy'
+        'token_entropy': 'Entropy',
+        'random_select_crf': 'Random+CRF',
+        'least_confidence_crf': 'Least+CRF',
+        'token_entropy_crf': 'Entropy+CRF'
     }
 
     # Find all .npy files
@@ -47,4 +64,5 @@ if __name__ == '__main__':
             files.append(f)
             labels.append(label_map[file_name])
 
-    plot(files, model_dir, labels)
+    lineplot(files, model_dir, labels)
+    barplot(labels)
